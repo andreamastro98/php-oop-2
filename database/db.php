1efac07d71e1547589d2f3c805cae4db4c4dc507
andreamastro98 <andreamastrovito98@gmail.com>
2023-06-01 17:24:13 +0200

    modifica alla struttura dei prodotti

--- database/db.php
+++ database/db.php
@@ -1,18 +1,12 @@
 <?php
 
 require_once __DIR__ . '/../models/products.php';
-require_once __DIR__ . '/../models/dogProduct.php';
-require_once __DIR__ . '/../models/catProduct.php';
-require_once __DIR__ . '/../models/food.php';
-require_once __DIR__ . '/../models/giochi.php';
-require_once __DIR__ . '/../models/cucce.php';
+require_once __DIR__ . '/../models/productType.php';
+require_once __DIR__ . '/../models/animale.php';
 
 
 $arrayProducts = [
-    new product('Articolo per cane','', [new dogProduct( new food('croccantini','7$'),'','')]),
-    new product('Articolo per cane','', [new dogProduct('', new giochi('osso', '15$'),'')]),
-    new product('', 'articolo per gatto' [new catProduct(new food('croccantini', '7$'),'','')]),
-    new product('', 'articolo per gatto'[new catProduct('',new giochi('gomitolo di gomma', '15$'), '')]),
+    new product([new ProductType('croccantini','')], 'cibo',[new animale('cane','')])
 ];
 
 var_dump($arrayProducts)
